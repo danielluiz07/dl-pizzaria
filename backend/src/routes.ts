9baf3a10ed2ba8d1dcb13dbe757b88385c8d170d
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-10 12:48:34 -0300

    Criacao do category Schema

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,7 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { isAdmin } from "./middlewares/isAdmin";
+import { createCategorySchema } from "./schemas/categorySchema";
 
 const router = Router()
 
@@ -18,7 +19,7 @@ router.post("/session", validateSchema(authUserSchema), new AuthUserController()
 router.get("/me", isAuthenticated , new DetailUserController().handle)
 
 //Rota Category
-router.post("/category", isAuthenticated, isAdmin, new CreateCategoryController().handle)
+router.post("/category", isAuthenticated, isAdmin,validateSchema(createCategorySchema) ,new CreateCategoryController().handle)
 
 export { router }
 
---
+++ backend/src/schemas/categorySchema.ts
@@ -0,0 +1,8 @@
+import {z} from "zod";
+
+export const createCategorySchema = z.object({
+    body: z.object({
+        name: z.string({message: "Categoria precisa ser um texto"})
+        .min(2, {message: "Nome da categoria precisa ter pelo menos 2 caracteres"})
+    })
+})
