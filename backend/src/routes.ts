dfd5ce302dd0cc5d94b0c46a5093b430260aed65
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-10 13:17:37 -0300

    Criacao da listagem de categoria - Controller, Service e router

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,27 +1,45 @@
 import { Router } from "express";
-import { CreateUserController } from "./controllers/user/CreateUserController";
-import { validateSchema } from "./middlewares/validateSchema";
-import { createUserSchema, authUserSchema} from "./schemas/userSchema";
-import { AuthUserController } from "./controllers/user/AuthUserController";
-import { DetailUserController } from "./controllers/user/DetailUserController";
-import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAdmin } from "./middlewares/isAdmin";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { validateSchema } from "./middlewares/validateSchema";
 import { createCategorySchema } from "./schemas/categorySchema";
+import { authUserSchema, createUserSchema } from "./schemas/userSchema";
 
-const router = Router()
+const router = Router();
 
-//Rotas users
-router.post("/users", validateSchema(createUserSchema) ,new CreateUserController().handle)
+// Rotas users
+router.post(
+  "/users",
+  validateSchema(createUserSchema),
+  new CreateUserController().handle
+);
 
-router.post("/session", validateSchema(authUserSchema), new AuthUserController().handle)
+router.post(
+  "/session",
+  validateSchema(authUserSchema),
+  new AuthUserController().handle
+);
 
-router.get("/me", isAuthenticated , new DetailUserController().handle)
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
-//Rota Category
-router.post("/category", isAuthenticated, isAdmin,validateSchema(createCategorySchema) ,new CreateCategoryController().handle)
+// Rotas Category
+router.post(
+  "/category",
+  isAuthenticated,
+  isAdmin,
+  validateSchema(createCategorySchema),
+  new CreateCategoryController().handle
+);
 
-export { router }
+router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+export { router };
+
 
 //Arquiterura em Camadas:
 //  Routes - Controller - Service
