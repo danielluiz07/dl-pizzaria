b10a42f7d520aa4857383cfce68cc67302a9b2ab
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-12 18:41:20 -0300

    modificacao no DeleteOrderController

--- backend/src/controllers/order/DeleteOrderController.ts
+++ backend/src/controllers/order/DeleteOrderController.ts
@@ -3,12 +3,12 @@ import { DeleteOrderService } from "../../services/order/DeleteOrderService";
 
 class DeleteOrderController{
     async handle(req: Request, res: Response){
-        const {order_id} = req.body
+        const order_id = req.query?.order_id as string
         
-        const DeleteOrder = new DeleteOrderService()
-        const updateOrder = await DeleteOrder.execute({order_id: order_id})
+        const deleteOrder = new DeleteOrderService()
+        const order = await deleteOrder.execute({order_id: order_id})
         
-        res.json(updateOrder)
+        res.json(order)
     }
 }
 
